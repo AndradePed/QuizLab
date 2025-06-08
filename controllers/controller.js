7a29db6cd1145ca7466f4bbc382511e6ae14f7f5
Pedro Henrique <167347682+AndradePed@users.noreply.github.com>
2025-06-08 20:26:39 -0300

    QuizLab

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,14 +1,54 @@
-const questaoModels = require('../models/quizModel');
 
-async function mostrarQuestoes() {
+const { error } = require('console');
+
+const QuestaoModel = require('../models/questaoModel');
+const AlternativaModel = require('../models/alternativaModel');
+
+
+exports.mostrarQuestoes = async (req, res) => {
     try {
-        const questoes = await questaoModels.listaDeQuestoes();
+        const questaoModel = new QuestaoModel({});
+        const questoes = await questaoModel.listaDeQuestoes();
         console.log('CONTROLLER OK')
         return questoes
     } catch (err) {
         console.log('Erro ao listar questões')
-        res.send(500).send('Erro ao listar Questões')
+        throw err;
     }
 }
 
-module.exports = { mostrarQuestoes }
+
+exports.criacaoQuestao = async (req, res) => {
+    try {
+        const questaoModel = new QuestaoModel(req.body);
+        const id_questao = await questaoModel.criacaoDeQuestao();
+
+        req.body.id_questao = id_questao.id
+
+        const alternativaModel = new AlternativaModel(req.body);
+        await alternativaModel.criacaoDeAlternativas();
+
+    } catch (err) {
+        console.log('Erro ao listar questões')
+        throw err;
+    }
+
+    // Agora somente fazer o controller para adicionar questão que está retornando o id e adicionar as alternativas (ficar de olho)
+    // (tratar possíveis problemas que podem vir depois de criar ou excluir questões, principalmente com o id)
+}
+
+
+exports.buscaQuestaoQuiz = async(req, res) => {
+    try {
+        const questaoModel = new QuestaoModel(req.body);
+        const dadosQuestao = await questaoModel.questaoSorteadaQuiz()
+
+
+
+        // Vou receber 
+
+    } catch(err) {
+        console.log('Erro ao buscar questão')
+        throw err;
+    }
+}
--- routes/router.js
+++ routes/router.js
@@ -8,19 +8,29 @@ router.get('/', (req, res) => {
 
 // LISTAR QUESTÕES
 
-const mostrarQuestoesController = require('../controllers/controller')
+const QuizController = require('../controllers/controller')
 
 router.get('/listarQuestoes', async (req, res) => {
-    try{
-        const questoes = await mostrarQuestoesController.mostrarQuestoes();
-        res.render('pages/listarQuestoes', { questoes })
-        console.log('ROUTER OK')
-        console.log(questoes)
-    } catch (err) {
-        console.log('Conexão falhou');
-        res.status(500).send('Erro ao listar questões');
+    try {
+        const questoes = await QuizController.mostrarQuestoes();
+        res.render('pages/listarQuestoes', {
+            questoes,
+            title: 'Questões'
+        })
+    } catch (err){
+        res.status(500).send('Busca mal-sucedida')
     }
     
+})
+
+
+router.get('/AdicionarQuestao', (req, res) => {
+    res.render('pages/criarQuestao')
+})
+
+router.post('/criarQuestao', async (req,res) => {
+    await QuizController.criacaoQuestao(req, res)
+    res.redirect('/listarQuestoes')
 })
 
 
